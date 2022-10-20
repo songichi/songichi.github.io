22fe962c011264b9d2c0f4f04a353ea3e5a803e4
songichi <99464969+songichi@users.noreply.github.com>
2022-10-20 22:52:02 +0800

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-import {primaryColor} from './homepage.js';
-import {secondaryColor} from './homepage.js';
-//import {typeName} from './homepage.js';
-
-sessionStorage.setItem("recentColor", primaryColor);
-sessionStorage.setItem("recentColorS", secondaryColor);
-//sessionStorage.setItem("typeName", typeName);
