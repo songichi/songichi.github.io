8094e5d9f260470603de3905bbddbe390e622377
songichi <99464969+songichi@users.noreply.github.com>
2022-10-22 23:01:14 +0800

    Add files via upload

--- typeData.js
+++ typeData.js
@@ -24,7 +24,7 @@ typeGroupList = [
     new typeObject(["hanzi"], "天人五衰", "type/tenningosui.png", true, "#ce1906", "#ffffff", "天人の五衰も目の前に見えてあさましや", "———『天人五衰』三島由紀夫", new Date(2022, 10, 4, 0, 0, 0, 0)),
     new typeObject(["hanzi"], "雷峯塔", "type/leifengta.png", false, "#f9e532", "#000000", "雷峰颓塔紫烟中、潦倒斜曛似醉翁", "", new Date(2022, 10, 4, 0, 0, 0, 0)),
     new typeObject(["hanzi"], "濃妝淡抹", "type/nongzhuangdanmo.png", true, "#f7f5e2", "#f25e2f", "欲把西湖比西子，淡妝濃抹總相宜", "———『飲湖上初晴後雨二首·其二』蘇軾", new Date(2022, 10, 4, 0, 0, 0, 0)),
-    new typeObject(["latin"], "HAMLET", "type/hamlet.png", false, "#f9e532", "#000000", "To be or not to be, that is the question", "———『Hamlet』William Shakespeare", new Date(2022, 10, 4, 0, 0, 0, 0)),
+    new typeObject(["latin"], "HAMLET", "type/hamlet.png", false, "#f9e532", "#000000", "O, from this time forth, my thoughts be bloody, or be nothing worth!", "———『Hamlet』William Shakespeare", new Date(2022, 10, 4, 0, 0, 0, 0)),
     new typeObject(["hanzi", "kana"], "吾輩は猫である", "type/wagahai.png", false, "#ed6d03", "#ffffff", "吾輩は猫である。名前はまだない。", "———『吾輩は猫である』夏目漱石", new Date(2022, 10, 4, 0, 0, 0, 0)),
     new typeObject(["hanzi"], "觀自在菩薩", "type/guanzizai.png", true, "#153513", "#ffffff", "觀自在菩薩，行深般若波羅蜜多時", "———『心經』", new Date(2022, 10, 4, 0, 0, 0, 0)),
     new typeObject(["hanzi"], "國殤", "type/guoshang.png", true, "#11f7af", "#000000", "操吳戈兮被犀甲，車錯轂兮短兵接", "———『國殤』屈原", new Date(2022, 10, 4, 0, 0, 0, 0)),
