b246d34d88afeab27ee41636e4443c40cfe4c1a2
songichi <99464969+songichi@users.noreply.github.com>
2022-11-21 00:00:59 +0800

    Update hanziData.js

--- app/hanziData.js
+++ app/hanziData.js
@@ -32,6 +32,7 @@ var hanziGroupList = [
     [new zi("在", 6, "在"), new zi("丄", 2, "在")],
     [new zi("再", 6, "再"), new zi("冄", 4, "再")],
     [new zi("能", 10, "能"), new zi("厸", 4, "能")],
+    [new zi("要", 9, "要"), new zi("ヌ", 4, "要")],
 ];
 export const hanziGroups = hanziGroupList;
 export { zi };
