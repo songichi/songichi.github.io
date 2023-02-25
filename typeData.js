5d41c10f342c5765814e9d8a2ff99885b36a6c90
songichi <99464969+songichi@users.noreply.github.com>
2023-02-25 01:47:12 -0500

    Update typeData.js

--- typeData.js
+++ typeData.js
@@ -15,6 +15,7 @@ class typeObject {
 var typeGroupList = [];
 //data for types
 typeGroupList = [
+    new typeObject(["hanzi"], "飲中八仙歌", "type/yinzhongbaxiange.png", true, "#FF2D58", "#FCFFD4", "李白一斗詩百篇、長安市上酒家眠", "———『飲中八仙歌』杜甫", new Date(2023, 1, 25, 1, 46, 0, 0)),
     new typeObject(["hanzi"], "陳勝吳廣", "type/chenshengwuguang.png", false, "#F7F0E6", "#61291C", "王侯將相寧有種乎", "———『史記*陳涉世家』司馬遷", new Date(2023, 1, 4, 14, 15, 0, 0)),
     new typeObject(["hanzi"], "落花流水", "type/luohualiushui.png", true, "#FBFCDD", "#E05D23", "水點蒸發變做白雲、花瓣飄落下游生根", "———『落花流水』黃偉文", new Date(2023, 1, 2, 1, 0, 0, 0)),
     new typeObject(["hanzi"], "相思", "type/xiangsi.png", true, "#F3E0B1", "#9C1F46", "紅豆生南國、春來發幾枝", "———『相思』王維", new Date(2023, 0, 27, 21, 25, 0, 0)),
