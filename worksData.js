8fca0f2bab5552a319636eb4ff34d7f6edcb4c6a
songichi <99464969+songichi@users.noreply.github.com>
2023-10-11 15:49:46 -0400

    Add files via upload

--- worksData.js
+++ worksData.js
@@ -14,7 +14,7 @@ class worksObject {
 var worksGroupList = [];
 worksGroupList = [
     //new worksObject("Carraway", "西文字体 | Latin Typeface", false, "中信大方", "works/Carraway_typeface.png", 5, new Date(2023, 2, 1, 20, 19, 0, 0), "封面设计，内书为新潮社『仮面の告白』"),
-    new worksObject("拒绝所有的岸", "主视觉海报 | Key Visual", false, "中信大方", "works/jujuesuoyoudean.png", 21.6, new Date(2023, 2, 1, 20, 19, 0, 0), "封面设计，内书为新潮社『仮面の告白』"),
+    new worksObject("拒绝所有的岸", "主视觉海报 | Key Visual", true, "中信大方", "works/jujuesuoyoudean.png", 21.6, new Date(2023, 2, 1, 20, 19, 0, 0), "封面设计，内书为新潮社『仮面の告白』"),
     new worksObject("仮面の告白", "书籍封面 | Book Cover", true, "", "works/kamennokokuhaku_book.png", 15, new Date(2023, 2, 1, 20, 19, 0, 0), "封面设计，内书为新潮社『仮面の告白』"),
     new worksObject("1922：文学奇迹之年", "主视觉海报 | Key Visual", false, "中信大方", "works/1922.png", 21, new Date(2023, 2, 1, 20, 19, 0, 0), ""),
     new worksObject("去海上吧！2022西葡语文学节", "主视觉海报 | Key Visual", false, "中信大方", "works/quhaishangba.png", 22, new Date(2023, 2, 1, 20, 19, 0, 0), ""),
