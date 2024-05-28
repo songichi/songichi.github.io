1b15069d087794a84bd2c26ff25a6c048f2d1a73
songichi <99464969+songichi@users.noreply.github.com>
2024-05-28 16:20:28 +0800

    Update worksData.js

--- worksData.js
+++ worksData.js
@@ -13,7 +13,8 @@ class worksObject {
 }
 var worksGroupList = [];
 worksGroupList = [
-    //new worksObject("Carraway", "西文字体 | Latin Typeface", false, "中信大方", "works/Carraway_typeface.png", 5, new Date(2023, 2, 1, 20, 19, 0, 0), "封面设计，内书为新潮社『仮面の告白』"),
+    //new worksObject("Carraway", "西文字体 | Latin Typeface", false, "中信大方", "works/Carraway_typeface.png", 10, new Date(2024, 5, 28, 16, 20, 0, 0), "封面设计"),
+    new worksObject("肖邦夜曲", "唱片封面 | Key Visual", false, "", "works/chopin_nocturnes.png", 21.6, new Date(2023, 2, 1, 20, 19, 0, 0), "封面设计，内书为新潮社『仮面の告白』"),
     new worksObject("拒绝所有的岸", "主视觉海报 | Key Visual", false, "中信大方", "works/jujuesuoyoudean.png", 21.6, new Date(2023, 2, 1, 20, 19, 0, 0), "封面设计，内书为新潮社『仮面の告白』"),
     new worksObject("仮面の告白", "书籍封面 | Book Cover", true, "", "works/kamennokokuhaku_book.png", 15, new Date(2023, 2, 1, 20, 19, 0, 0), "封面设计，内书为新潮社『仮面の告白』"),
     new worksObject("1922：文学奇迹之年", "主视觉海报 | Key Visual", false, "中信大方", "works/1922.png", 21, new Date(2023, 2, 1, 20, 19, 0, 0), ""),
